498be4f1111c4da20ad517a6f0fa6e6a04218961
sugarcothe <ifeanyivalentine82@gmail.com>
2021-10-11 07:41:42 -0700

    navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -4,7 +4,14 @@ import './Navbar.scss';
 const Navbar = () => {
     return (
         <div className='navbar'>
-
+            <div className='sections'>
+                <div className='puellae'>
+                    <i class="fas fa-venus"></i>
+                </div>
+                <div className='pueri'>
+                    <i class="fas fa-mars"></i>
+                </div>
+            </div>
         </div>
     )
 }
